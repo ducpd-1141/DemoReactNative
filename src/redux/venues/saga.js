333aa5c18575dea59126d4283912322579d30390
phongvhd93 <phongvhd93@gmail.com>
2019-04-16 16:52:11 +0700

    Add recommend venues API + table cells

--- src/redux/venues/saga.js
+++ src/redux/venues/saga.js
@@ -1,6 +1,7 @@
-import { takeEvery } from 'redux-saga/effects';
-// import { CLIENT_ID, CLIENT_SECRET } from '../constants/index';
+import { takeEvery, call, put } from 'redux-saga/effects';
 import { actionTypes } from './actions';
+import api from '../../network';
+import { getApiPath } from '../../constants';
 
 function increment() {
   console.log('This is increment saga');
@@ -10,9 +11,26 @@ function decrement() {
   console.log('This is decrement saga');
 }
 
+function* fetchRecommendVenues(action) {
+  try {
+    const json = yield call(
+      api.get,
+      getApiPath(`venues/explore?ll=${action.payload.lat},${action.payload.lng}&limit=5`),
+    );
+    const { meta, response } = json.data;
+    if (meta && meta.code === 200) {
+      yield put({ type: actionTypes.receivedRecommendVenues, response });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
+}
+
 const venuesSaga = [
-  takeEvery(actionTypes.INCREMENT, increment),
-  takeEvery(actionTypes.DECREMENT, decrement),
+  takeEvery(actionTypes.increment, increment),
+  takeEvery(actionTypes.decrement, decrement),
+  takeEvery(actionTypes.fetchRecommendVenues, fetchRecommendVenues),
 ];
 
 export default venuesSaga;
